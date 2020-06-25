d22e0eeb91c3db8d138ac509eee48efe69fbe378
Steven Clontz <steven.clontz@gmail.com>
2020-06-25 21:30:10 +0000

    factor out code listings

---
+++ source/files/programming-jupyter-use-tempdata.py
@@ -0,0 +1,6 @@
+with open("data.csv", "r") as csv_file:
+    rows = csv.DictReader(csv_file, delimiter=',')
+    for row in rows:
+        time = row["Time"]
+        temp = format((float(row["TempInF"])-32)*5/9, '.2f')
+        print("The temperature at", time, "is", temp, "degrees Celcius.")
