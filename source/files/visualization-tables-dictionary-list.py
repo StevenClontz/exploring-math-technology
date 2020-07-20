8e6ac4808acce3ae51d90c3f5b982806258f80e1
Steven Clontz <steven.clontz@gmail.com>
2020-07-20 21:36:34 +0000

    tables activity

---
+++ source/files/visualization-tables-dictionary-list.py
@@ -0,0 +1,17 @@
+[
+    {
+        "name":             "Jessica",
+        "age":              19,
+        "favorite_teacher": "Dr. Clontz",
+    },
+    {
+        "name":             "Lynn",
+        "age":              18,
+        "favorite_teacher": "Ms. Gilbreath",
+    },
+    {
+        "name":             "Julia",
+        "age":              17,
+        "favorite_teacher": "Mr. Fullinwider",
+    },
+]
---
+++ source/files/visualization-tables-from-rows.py
@@ -0,0 +1,5 @@
+Table(labels=["Name", "Age", "Favorite Teacher"]).with_rows([
+    ["Jessica", 19, "Dr. Clontz"],
+    ["Lynn", 18, "Ms. Gilbreath"],
+    ["Julia", 17, "Mr. Fullinwider"],
+])
