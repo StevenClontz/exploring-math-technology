9e9395349741d68a8feb6bc888fedd5b19bc03fd
Steven Clontz <steven.clontz@gmail.com>
2020-07-23 18:10:46 +0000

    zero-pad left

--- jupyter.py
+++ jupyter.py
@@ -26,16 +26,14 @@ def build():
     make_directory(jo_directory_indv)
     for cindex, chapter in enumerate(book.xpath("//chapter"), start=1):
         if chapter.xpath("@xml:id"):
-            jo_directory_team_ch = jo_directory_team + "/chapter-" + str(cindex) + "-" + chapter.xpath('./@xml:id')[0]
-            jo_directory_indv_ch = jo_directory_indv + "/chapter-" + str(cindex) + "-" + chapter.xpath('./@xml:id')[0]
+            jo_directory_team_ch = f"{jo_directory_team}/chapter-{cindex:02d}-{chapter.xpath('@xml:id')[0]}"
+            jo_directory_indv_ch = f"{jo_directory_indv}/chapter-{cindex:02d}-{chapter.xpath('@xml:id')[0]}"
             make_directory(jo_directory_team_ch)
             make_directory(jo_directory_indv_ch)
             for sindex, section in enumerate(chapter.xpath("section"), start=1):
                 if section.xpath("@xml:id"):
-                    team_section_path = jo_directory_team_ch +"/section-" + str(cindex) + "-" + str(
-                        sindex) + "-" + section.xpath('./@xml:id')[0] + ".ipynb"
-                    indv_section_path = jo_directory_indv_ch +"/section-" + str(cindex) + "-" + str(
-                        sindex) + "-" + section.xpath('./@xml:id')[0] + ".ipynb"
+                    team_section_path = f"{jo_directory_team_ch}/section-{cindex}-{sindex}-{section.xpath('@xml:id')[0]}.ipynb"
+                    indv_section_path = f"{jo_directory_indv_ch}/section-{cindex}-{sindex}-{section.xpath('@xml:id')[0]}.ipynb"
                     section_code = "'" + str(cindex) + "." + str(sindex) + "'"
                     transform(
                         section,
