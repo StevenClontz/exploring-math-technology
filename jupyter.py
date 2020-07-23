f752c096f078035678e2518277bd7fd486900dd5
Steven Clontz <steven.clontz@gmail.com>
2020-07-23 18:02:59 +0000

    build separate folders for each chapter

--- jupyter.py
+++ jupyter.py
@@ -25,20 +25,26 @@ def build():
     make_directory(jo_directory_team)
     make_directory(jo_directory_indv)
     for cindex, chapter in enumerate(book.xpath("//chapter"), start=1):
-        for sindex, section in enumerate(chapter.xpath("section"), start=1):
-            team_section_path = jo_directory_team +"/section-" + str(cindex) + "-" + str(
-                sindex) + "-" + section.xpath('./@xml:id')[0] + ".ipynb"
-            indv_section_path = jo_directory_indv +"/section-" + str(cindex) + "-" + str(
-                sindex) + "-" + section.xpath('./@xml:id')[0] + ".ipynb"
-            section_code = "'" + str(cindex) + "." + str(sindex) + "'"
-            transform(
-                section,
-                section=section_code,
-                mode="'team'").write_output(team_section_path)
-            transform(
-                section,
-                section=section_code,
-                mode="'individual'").write_output(indv_section_path)
+        if chapter.xpath("@xml:id"):
+            jo_directory_team_ch = jo_directory_team + "/chapter-" + str(cindex) + "-" + chapter.xpath('./@xml:id')[0]
+            jo_directory_indv_ch = jo_directory_indv + "/chapter-" + str(cindex) + "-" + chapter.xpath('./@xml:id')[0]
+            make_directory(jo_directory_team_ch)
+            make_directory(jo_directory_indv_ch)
+            for sindex, section in enumerate(chapter.xpath("section"), start=1):
+                if section.xpath("@xml:id"):
+                    team_section_path = jo_directory_team_ch +"/section-" + str(cindex) + "-" + str(
+                        sindex) + "-" + section.xpath('./@xml:id')[0] + ".ipynb"
+                    indv_section_path = jo_directory_indv_ch +"/section-" + str(cindex) + "-" + str(
+                        sindex) + "-" + section.xpath('./@xml:id')[0] + ".ipynb"
+                    section_code = "'" + str(cindex) + "." + str(sindex) + "'"
+                    transform(
+                        section,
+                        section=section_code,
+                        mode="'team'").write_output(team_section_path)
+                    transform(
+                        section,
+                        section=section_code,
+                        mode="'individual'").write_output(indv_section_path)
     #if not os.path.exists("activities/images"):
     #  copytree("apc/src/images","activities/images")
     subprocess.run(['pretext','build'])
