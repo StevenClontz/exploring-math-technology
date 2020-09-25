fb61bc6ee9138b611bc5c5bd8d52103500c74d34
Steven Clontz <steven.clontz@gmail.com>
2020-09-25 16:53:53 +0000

    refactor fizzbuzz to Parson problem

---
+++ source/files/data-programming-fizzbuzz-scrambled.py
@@ -0,0 +1,11 @@
+fizz = ""
+print(f"{number}{fizz}{buzz}")
+fizz = " Fizz"
+if number % 3 == 0: # if number is divisible by three
+buzz = ""
+else:
+buzz = " Buzz"
+max_number = 100
+else:
+for number in range(1,max_number+1):
+if number % 5 == 0:
