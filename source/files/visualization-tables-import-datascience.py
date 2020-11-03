7782196cf6091a804d0f6a07e18adf016f86781e
Steven Clontz <steven.clontz@gmail.com>
2020-11-03 21:27:42 +0000

    add kernel restart instructions

--- source/files/visualization-tables-import-datascience.py
+++ source/files/visualization-tables-import-datascience.py
@@ -4,8 +4,11 @@ try:
 except ModuleNotFoundError:
     import sys
     print("The module `datascience` is not installed.")
-    print("To install it for your CoCalc project, use the [(+) New] menu to")
-    print("open a Linux terminal.")
-    print("Then copy-paste the line below into the terminal and hit [Enter].")
+    print("To install it for your CoCalc project, do the following:"
+    print("1. Use the [(+) New] menu to open a Linux terminal.")
+    print("2. Copy-paste the line below into the terminal and hit [Enter].")
     print()
     print(f"{sys.executable} -m pip install --user datascience")
+    print()
+    print("3. Return to this notebook and \"Restart Kernel\".")
+    print("4. Re-run this Code cell.")
